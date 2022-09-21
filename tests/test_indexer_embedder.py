04631f7ac3f789119b471f72ada8f43ffdab3620
Zhaoyang Xie <zxie@etscanada.ca>
2022-09-21 09:37:39 -0400

    modify token indexer_embedder unit test

--- tests/test_indexer_embedder.py
+++ tests/test_indexer_embedder.py
@@ -7,12 +7,10 @@ from allennlp.data.tokenizers import WhitespaceTokenizer
 from allennlp.data.vocabulary import Vocabulary
 import unittest
 from torch import LongTensor
-import pudb
 
 
 class Test_indexer_embedder(unittest.TestCase):
     def test_tokens_to_indices(self):
-        pudb.set_trace()
         # initialize for indexer and embedder
         token_embedder = BasicTextFieldEmbedder({'bert': PretrainedBertEmbedder(pretrained_model='roberta-base',
 
@@ -28,6 +26,7 @@ class Test_indexer_embedder(unittest.TestCase):
 
         # initialize WhitespaceTokenizer                                      
         tokenizer = WhitespaceTokenizer()
+        # initialize parameters and inputs
         sentence = "the Quick brown fox jumped over the laziest lazy elmo"
         vocab_path = "../data/output_vocabulary"
         tokens = tokenizer.tokenize(sentence)
@@ -39,10 +38,12 @@ class Test_indexer_embedder(unittest.TestCase):
         inputs = token_indexer.tokens_to_indices(tokens, vocab)
         # add paddings so that all tensor have same size
         padded_tokens = token_indexer.as_padded_tensor_dict(inputs,padding_lengths)
-        reshaped_padded_tokens = {}
+        # add additional wrapping for the token embedder
+        reshaped_padded_tokens = {'bert': {}}
         # reshape padded_tokens to be in the batch format. (since we only pass one sentence in)
         for key, value in padded_tokens.items():
-            reshaped_padded_tokens[key] = torch.reshape(value, (1, value.size(0)))
+            reshaped_padded_tokens['bert'].update({key: torch.reshape(value, (1, value.size(0)))})
+
 
         token_embedder(reshaped_padded_tokens)
 if __name__ == "__main__":
